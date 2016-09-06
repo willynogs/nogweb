bfed1a5cb410f1253269cc1647843fd2daa6f067
willynogs <willynogs@gmail.com>
2016-09-05 22:22:06 -0400

    add click circle

--- js/app.js
+++ js/app.js
@@ -11,6 +11,7 @@ $(function(){
 
     if($('.overlay-menu').hasClass('overlay-menu-active')) {
         $('.overlay-menu').fadeOut(200).toggleClass('overlay-menu-active');
+        $('.click-circle').fadeOut(200);
     } else {
       $('.overlay-menu').show().toggleClass('overlay-menu-active');
     }
@@ -27,12 +28,34 @@ $(function(){
   $('.overlay-link').click(function(){
     var x = event.pageX - 5;
     var y = event.pageY - 5;
-    $('body').append("<div class='click-circle' style='top:" + y + "px;left:" + x + "px;' data-top='" + y + "px' data-left='" + x + "px'></div>");
+    var pageHeight = $(window).height();
+    var pageWidth = $(window).width();
+    //default r of 200
+    var r = 200;
+    //CIRCLE RADIUS SHOULD BE EQUAL TO FARTHEST DIAGONAL FROM CENTER
+    if ((pageWidth / 2) > x) {
+      console.log("first half");
+      d = (Math.sqrt((Math.pow((pageWidth - x), 2) + Math.pow((pageHeight / 2), 2))) * 2);
+    } else {
+      console.log("second half");
+      var newX = pageWidth - x;
+      var newY = pageHeight - y;
+      d = (Math.sqrt((Math.pow((pageWidth - newX), 2) + Math.pow((pageHeight / 2), 2))) * 2) + 50;
+    }
+    $('body').append("<div class='click-circle' style='top:" + y + "px;left:" + x + "px;'></div>");
     $('.click-circle').animate({
-      height: "200px",
-      width: "200px",
-      top: (y - 100) + "px",
-      left: (x - 100) + "px"
-    }, 1000);
+      height: d + "px",
+      width: d + "px",
+      top: (y - (d / 2)) + "px",
+      left: (x - (d / 2)) + "px"
+    }, 800, function(){
+      $(this).css({
+        "height": "100vh",
+        "width": "100vw",
+        "top": "0px",
+        "left": "0px",
+        "border-radius": "0"
+      });
+    });
   });
 });
